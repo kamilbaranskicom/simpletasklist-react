7e403d1d4a016f8bf9fe339eeebcfac1be69f035
kb <kb@macbookkb.local>
2020-07-24 03:08:44 +0200

    Updates

---
+++ precache-manifest.1214cde298c4bbc4de5a9199c599dedd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259de7ed9c60f3010f871392b73c368e",
+    "url": "/simpletasklist-react/index.html"
+  },
+  {
+    "revision": "64a45ebaeb7ec673fdb7",
+    "url": "/simpletasklist-react/static/css/main.0466656e.chunk.css"
+  },
+  {
+    "revision": "d96bc0e6e7fd82c66417",
+    "url": "/simpletasklist-react/static/js/2.a2eeb372.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simpletasklist-react/static/js/2.a2eeb372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a45ebaeb7ec673fdb7",
+    "url": "/simpletasklist-react/static/js/main.80f35e55.chunk.js"
+  },
+  {
+    "revision": "918441f4957f32b955d4",
+    "url": "/simpletasklist-react/static/js/runtime-main.04dc6f63.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simpleTaskList-react/precache-manifest.dcdb623242b38eff48b3d048080a064f.js"
+  "/simpletasklist-react/precache-manifest.1214cde298c4bbc4de5a9199c599dedd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simpleTaskList-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simpletasklist-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
